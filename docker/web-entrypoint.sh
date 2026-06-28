1e29c3ade9c531855c20c73145e1d1ccf0d79170
Fernando Barrera Melchor <47169938+barreramelchorf@users.noreply.github.com>
2026-06-28 12:19:33 -0600

    fix: entrypoint always replaces placeholder (even with empty string)

--- docker/web-entrypoint.sh
+++ docker/web-entrypoint.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 # Replace build-time placeholder with runtime env var in all JS files
-# This allows NEXT_PUBLIC_* vars to be set at deploy time, not build time
-
 PLACEHOLDER="__NEXT_PUBLIC_API_URL_PLACEHOLDER__"
 ACTUAL="${NEXT_PUBLIC_API_URL:-}"
 
-if [ -n "$ACTUAL" ]; then
-  find /app/apps/web/.next -name "*.js" -exec sed -i "s|$PLACEHOLDER|$ACTUAL|g" {} +
-fi
+# Always replace - even if ACTUAL is empty (removes the placeholder)
+find /app/apps/web/.next -name "*.js" -exec sed -i "s|$PLACEHOLDER|$ACTUAL|g" {} +
 
 exec node apps/web/server.js
